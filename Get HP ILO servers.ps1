3d2bf1fb12dc2c197122934d06d9f3c384358126
bgelov <belov@ymail.com>
2023-06-07 23:26:40 -0300

    Add scripts

---
+++ Get HP ILO servers.ps1
@@ -0,0 +1,76 @@
+﻿# Get HP ILO servers
+# You can use one of this variants
+
+# 1
+$srvILO2 = Find-HPiLO 10.1.1.1-255
+$srvILO2 | Export-Csv D:\ILO\srvilo2.csv -Encoding UTF8
+
+
+# 2
+$username = "Administrator"
+$password = read-host "Enter ILO password"
+Get-HPiLOHealthSummary -Server srv2-ilo -Username $username -Password $password -DisableCertificateAuthentication
+
+IP                 : *******
+HOSTNAME           : srv2-ilo
+STATUS_TYPE        : OK
+STATUS_MESSAGE     : OK
+DRIVE_STATUS       : OK
+FANS               : {@{STATUS=OK}, @{REDUNDANCY=Redundant}}
+POWER_SUPPLIES     : {@{STATUS=OK}, @{REDUNDANCY=Redundant}}
+TEMPERATURE_STATUS : OK
+VRM_STATUS         : OK
+
+
+# 3
+$username = "Administrator"
+$password = read-host "Enter ILO password"
+$str = "FQDN;IP;HOSTNAME;ILO_FIRMWARE_VERSION;FIRMWARE_DATE;SPN;PN;SerialNumber;UUID;DRIVES_PRODUCT_ID;MEMORY_STATUS;STORAGE_STATUS;FANS_STATUS;POWER_SUPPLIES_STATUS;PROCESSOR_STATUS;BIOS_HARDWARE_STATUS;NETWORK_STATUS;TEMPERATURE_STATUS
+"
+$srvILO = Import-Csv "D:\ILO\srvilo.csv"
+foreach ($sILO in $srvILO) {
+
+    $srv_IP = $sILO.IP
+    $srv_HOSTNAME = $sILO.HOSTNAME
+    $srv_SPN = $sILO.SPN
+    $srv_FWRI = $sILO.FWRI
+    $srv_PN = $sILO.PN
+    $srv_SerialNumber = $sILO.SerialNumber
+    $srv_UUID = $sILO.UUID
+
+    $drive_pid = ""
+    $srv_fqdn = ""
+    $ilo_firm = ""
+    $ilo_firm_v = ""
+    $ilo_firm_date = ""
+    $srv_status = ""
+    $srvInfo = ""
+
+    $srvInfo = Get-HPiLOServerInfo -Server $sILO -Username $username -Password $password -DisableCertificateAuthentication
+
+    $drive_pid = $srvInfo.DRIVE.BACKPLANE.PRODUCT_ID
+    $drive_pid = $drive_pid.PRODUCT_ID
+
+    $srv_status = $srvInfo.HEALTH_STATUS
+
+    $srv_health_memory = $srv_status.MEMORY_STATUS
+    $srv_storage_status = $srv_status.STORAGE_STATUS
+    $srv_fans_status = $srv_status.FANS.STATUS
+    $srv_power_supplies_status = $srv_status.POWER_SUPPLIES.STATUS
+    $srv_processor_status = $srv_status.PROCESSOR_STATUS
+    $srv_bios_hardware_status = $srv_status.BIOS_HARDWARE_STATUS
+    $srv_network_status = $srv_status.NETWORK_STATUS
+    $srv_temp_status = $srv_status.TEMPERATURE_STATUS
+
+    $srv_fqdn = (Get-HPiLOServerFQDN -Server $sILO -Username $username -Password $password -DisableCertificateAuthentication).SERVER_FQDN
+
+    $ilo_firm = Get-HPiLOFirmwareVersion -Server $sILO -Username $username -Password $password -DisableCertificateAuthentication | select FIRMWARE_VERSION, FIRMWARE_DATE
+    #$ilo_firm_v = $ilo_firm.FIRMWARE_VERSION
+    $ilo_firm_date = $ilo_firm.FIRMWARE_DATE
+
+    $str += "$srv_fqdn;$srv_IP;$srv_HOSTNAME;$srv_FWRI;$ilo_firm_date;$srv_SPN;$srv_PN;$srv_SerialNumber;$srv_UUID;$drive_pid;$srv_health_memory;$srv_storage_status;$srv_fans_status;$srv_power_supplies_status;$srv_processor_status;$srv_bios_hardware_status;$srv_network_status;$srv_temp_status
+"
+
+}
+$str
+$str > "d:\ilo\1.csv"
---
+++ Upgrade HP ILO firmware.ps1
@@ -0,0 +1,50 @@
+﻿# Upgrade HP ILO firmware 
+
+$srv = Get-Content "\\fileserver\ILO\ilosrv.txt"
+$username = "Administrator"
+$password = read-host "Enter ILO password"
+
+$ilo_3_pre = 1.28
+$ilo_3_pre_fwr = "\\fileserver\HP\ilo\ilo3_128.bin"
+$ilo_3_p = $ilo_3_pre - 0.01
+$ilo_3_last = 1.89
+$ilo_3_last_fwr = "\\fileserver\HP\ilo\ilo3_189.bin"
+$ilo_4_last = 2.60
+$ilo_4_last_fwr = "\\fileserver\HP\ilo\ilo4_260.bin"
+
+$srvILO = Find-HPiLO $srv | Select IP, HOSTNAME, PN, FWRI | Sort PN, FWRI 
+foreach ($sILO in $srvILO) {
+
+    $sInfo = $sILO.IP + " - " + $sILO.HOSTNAME + " - " + $sILO.PN + " v " + $sILO.FWRI
+    Write-Host "Check server $sInfo"
+
+#ILO 3
+    if (($sILO.PN -like "*iLO 3*") -and ($sILO.FWRI -lt $ilo_3_last)) {
+        if ($sILO.FWRI -lt $ilo_3_pre) {
+
+            Write-Host "Update process running. ILO 3 $ilo_3_pre..." -ForegroundColor Yellow
+            $sILO | Update-HPiLOFirmware -Username $username -Password $password -DisableCertificateAuthentication -Location $ilo_3_pre_fwr
+            Write-Host "Update process has been finished! Please, run the script again for ILO 3 1.89 update" -ForegroundColor Yellow
+
+        } elseif (($sILO.FWRI -gt $ilo_3_p) -and ($sILO.FWRI -lt $ilo_3_last)) {
+            
+            Write-Host "Update process running. ILO 3 $ilo_3_last..." -ForegroundColor Yellow
+            $sILO | Update-HPiLOFirmware -Username $username -Password $password -DisableCertificateAuthentication -Location $ilo_3_last_fwr
+            Write-Host "Update process has been finished!" -ForegroundColor Yellow
+
+        }
+    }
+
+#ILO 4
+    if (($sILO.PN -like "*iLO 4*") -and ($sILO.FWRI -lt $ilo_4_last)) {
+
+        
+        Write-Host "Update process running. ILO 4 $ilo_4_last..." -ForegroundColor Yellow
+        $sILO | Update-HPiLOFirmware -Username $username -Password $password -DisableCertificateAuthentication -Location $ilo_4_last_fwr
+        Write-Host "Update process has been finished!" -ForegroundColor Yellow
+
+    }
+
+} #foreach
+
+Find-HPiLO $srv | Select HOSTNAME, PN, FWRI | Sort PN, FWRI
